64a137bde7bc3a038e831f88d1c9021ac92ae1ab
Trenton Edwards <39540197+TrentonGage11@users.noreply.github.com>
2021-12-16 22:32:16 -0500

    Update commands.lua

--- utils/commands.lua
+++ utils/commands.lua
@@ -1,6 +1,6 @@
-require("Resources/Server/PlayerManager/utils/utils")
-require("Resources/Server/PlayerManager/utils/permissions")
-local permissionsModule = loadPermissionsModule()
+require("Resources/Server/TorgCore/utils/utils")
+require("Resources/Server/TorgCore/utils/permissions")
+local permissions = loadPermissionsModule()
 local Module = {}
 -- Command Handler
 function decider(authID, authName, raw_message)
@@ -11,28 +11,38 @@ function decider(authID, authName, raw_message)
 		command = command_strings[1]
 		if command == "help" then
 			help(authID, command_strings[2])
-		--else if 
+		elseif command == "kick" then
+			kick(command_strings[2], authID, command_strings[3])
+		else
+			SendChatMessage(authID, "^4Error!^c Unknown command: ^r"..command)
 		end
 		return 0
 	end
+	return 1
 end
 
 -- Chat Commands 
 -- Help: /help [command]
-function help(authID, command)
---	if command == nil then
---		if 0 >= 0 then
-			SendChatMessage(authID, "^6<=^r ^3Commands for^r ^a[^6tg11^b.^2org^r^a]^r ^6=>^r")
-			SendChatMessage(authID, "^6/help ^2[command]^r")
-			SendChatMessage(authID, "^7 shows you this command^r")
-			SendChatMessage(authID, "^7 or help on another command^r")
---		end
---	end
+function help(authID, authName, command)
+	if command == nil then
+		SendChatMessage(authID, "^6<=^r ^3Commands for^r ^a[^6tg11^b.^2org^r^a]^r ^6=>^r")
+		SendChatMessage(authID, "^6/help ^2[command]^r")
+		SendChatMessage(authID, "^7 shows you this command^r")
+		SendChatMessage(authID, "^7 or help on another command^r")
+	elseif command == "kick" then
+		if check(permissions.moderators, authName) == true or check(permissions.administrators, authName) == true then
+			SendChatMessage(authID, "^6/kick ^a<user> ^2[reason]^r")
+		else
+			SendChatMessage(authID, "^4You are not permitted to run this command!^r")
+		end
+	else
+		SendChatMessage(authID, "^4Error!^c Unknown command: ^r"..command)
+	end
 end
 -- Kick: /kick <playerID> [message]
 function kick(kickID, authID, message)
 	name = GetPlayerName(kickID)
-	if 2 >= 2 then
+	if check(permissions.moderators, authName) == true or check(permissions.administrators, authName) == true then
 		DropPlayer(kickID, message)
 		SendChatMessage(-1, "The player `"..name.."` has been kicked from the server!")
 	else
@@ -41,9 +51,14 @@ function kick(kickID, authID, message)
 end
 
 
--- 
+
+local function loaded()
+	cmd_log("Loaded!")
+end
+
 function loadCommandsModule()
-	Module['decider'] = decider
-	Module['help'] = help
+	Module.decider = decider
+	Module.help = help
+	Module.loaded = loaded
 	return Module
 end
