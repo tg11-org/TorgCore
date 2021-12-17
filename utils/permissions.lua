5ca86985bbb5c863c105793f40d54041809ead0f
Trenton Edwards <39540197+TrentonGage11@users.noreply.github.com>
2021-12-16 22:32:31 -0500

    Update permissions.lua

--- utils/permissions.lua
+++ utils/permissions.lua
@@ -1,35 +1,27 @@
 require("Resources/Server/TorgCore/utils/utils")
-local Module = {}
+Module = {}
+permissions = {}
 permission = {group=nil, value=nil, members={}}
 function permission:new(group, value, members)
 	local _perm = permission
+	_perm.members = members
 	_perm.group = group
 	_perm.value = value
 	return _perm
-end
-administrator = permission:new("administrator", 2)
-moderator = permission:new("moderator", 1)
-member = permission:new("member", 0)
--- BeamMP Permissions
-function checkGroup(name)
-	if name == 'TrentonGage11' then
-		return administrator
-	else
-		return member
-	end
 end
 
 function parsePermissionsFile()
 	local raw_data = linesfrom("./Resources/Server/TorgCore/utils/permissions.yaml")
 	local data = {}
-	local index = 0
+	local index = 1
 	local temp = {}
 	for _,line in ipairs(raw_data) do
 		if line:endswith(':') then
+			temp = {}
 			data[index] = line:removelastchar()
 			index = index + 1
 		else
-			if line:startswith('  - ')
+			if line:startswith('  - ') then
 				line = line:sub(5)
 				_line = line:split(", ")
 				for _,line_ in ipairs(_line) do
@@ -37,16 +29,42 @@ function parsePermissionsFile()
 						temp[#temp+1] = line_
 					end
 				end
+				data[index] = temp
+				index = index + 1
 			end
 		end
 	end
+	return data
+end
+
+local function makePermissions()
+	local data = parsePermissionsFile()
+	administrator = permission:new()
+	administrator.group = "administrator"
+	administrator.value = 2
+	administrator.members = data[2]
+	moderator = permission:new()
+	moderator.group = "moderator"
+	moderator.value = 1
+	moderator.members = data[4]
+	member = permission:new()
+	member.group = "member"
+	member.value = 0
+	member.members = {}
+	permissions.administrator = administrator
+	permissions.moderator = moderator
+	permissions.member = member
+	Module.administrators = data[2]
+	Module.moderators = data[4]
+	return permissions
+end
+
+local function loaded()
+	perm_log("Loaded!")
 end
 
 function loadPermissionsModule()
-	Module.administrator = administrator
-	Module.permission = permission
-	Module.checkGroup = checkGroup
-	Module.moderator = moderator
-	Module.member = member
+	local permissions = makePermissions()
+	Module.loaded = loaded
 	return Module
 end
